d6cbb05fa878c0c9164d1635166db8f6c0aefda4
Taran <taz_dh@hotmail.com>
2024-11-27 16:34:54 +0000

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "04c25d75400d9f4d06e036610d9d5154
 "CNAME": "bf705ce32d351b2714fb34cf77fd604b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "9c5109ef4548999ba3e04f3c399ba304",
+"flutter_bootstrap.js": "0454e39752313e0717c37f606320634e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
