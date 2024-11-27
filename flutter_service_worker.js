6e5c2eedbb0ce319dea01c7ac6bfa4d16ec6272e
Taran <taran@moneysatnav.com>
2024-11-27 11:10:08 +0000

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "04c25d75400d9f4d06e036610d9d5154
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "c753bffd60643e10649b23a94a00b50a",
+"flutter_bootstrap.js": "766f4d49f05cc58c4f9febace21f2a1d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
