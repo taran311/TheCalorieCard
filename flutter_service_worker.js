4fecc728d01cdbbc17cbe9262f33d57d756134b6
Taran <taz_dh@hotmail.com>
2026-01-27 21:27:59 +0000

    Deploy Flutter web (clean root)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3364bace885012fbbabe07c6987a7e99
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "0f71e3c42b80c511a756c5ff3299cb97",
+"flutter_bootstrap.js": "07b0daaee8bfa3ce18cd7fd2ef40c9e3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
